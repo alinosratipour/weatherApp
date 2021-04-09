b51d8779aa09f7c8f060373469324bfd39071d2f
alinosratipour <alinosratipour@gmail.com>
2021-04-09 14:15:40 +0100

    connected weather api to my front-end and displayed degree and condition and weather summary

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ window.addEventListener('load', () =>{
 
 let long;
 let lat;
+let myIcon = document.querySelector(".icon");
+let temperatureDescription = document.querySelector(".temperature-description");
+let temperatureDegree= document.querySelector(".temperature-degree");
+let locationTimeZone = document.querySelector(".location-timeZone");
+ 
 
-if(navigator.geolocation){
+ if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(position =>{
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-  });
-}
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+ 
+    const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${long}?key=VFUVD9FGBHM9AE44HGFKYHRAQ`;
+  
+      fetch(api)
+      .then(response =>{
+      return response.json();
+      })
+      .then(data=>{
+        console.log(data);
+        const { temp, icon, conditions } = data.currentConditions;
+        const { description, timezone } = data;
+        //set dom elements from the api
+        temperatureDegree.textContent = temp;
+        temperatureDescription.textContent = description;
+        locationTimeZone.textContent = timezone;
+        myIcon.textContent = icon;
 
+      });
+    });
+   }
 });
